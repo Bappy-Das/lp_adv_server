16d5ecdc8d1bb0a0afbb258ac46a33460a495841
Bappy dase <bappydas.bd51@gmail.com>
2023-11-05 20:24:33 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+var colors = require('colors');
 const mongoose = require("mongoose");
+const connectToDatabase = require("./src/utils/dbConnect");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,14 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+
+try {
+    connectToDatabase()
+    app.listen(port, () => {
+        console.log(`server connected with listening port ${port}`.green.bold)
+    })
+
+} catch (error) {
+    console.log(error);
+}
 app.get("/", (req, res) => {
-    res.send("Route is working !")
+    res.send("Server is on running !")
 });
 
-app.listen(port, () => {
-    console.log(`lawer_panel_server app listening on port ${port}`)
-})
-
 app.all("*", (req, res) => {
-    res.send("NO route found.");
+    res.send("No route found.");
 });
--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -1,1 +1,21 @@
-console.log("DB will connect through this utils");
+const mongoose = require('mongoose');
+
+// Define a function to connect to the MongoDB database
+const connectToDatabase = async () => {
+    const mongoDBUrl = process.env.DATABASE_URL
+    try {
+        await mongoose.connect(mongoDBUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = mongoose.connection;
+        console.log("Database Conneted to server".red.bold);
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        db.once('open', () => {
+            console.log('Connected to the database');
+        });
+    } catch (error) {
+        console.error('An error occurred while connecting to the database:', error);
+    }
+};
+module.exports = connectToDatabase;
